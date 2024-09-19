6615e28a9c6beba20fa7223969e84ce874ee063d
ChiaXinLiang <cxldun@gmail.com>
2024-09-19 20:30:14 +0800

    add api wehook to public route

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,25 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)", "/"]);
+const isPublicRoute = createRouteMatcher([
+  // "/sign-in(.*)",
+  // "/sign-up(.*)",
+  "/",
+  "/api/webhook",
+  "question/:id",
+  "/tags",
+  "/tags/:id",
+  "/profile/:id",
+  "/community",
+  "/jobs",
+]);
+
+const ignoredRoutes = ["/api/webhook", "/api/chatgpt"];
 
 export default clerkMiddleware((auth, req) => {
-  if (!isPublicRoute(req)) {
+  if (
+    !isPublicRoute(req) &&
+    !ignoredRoutes.some((route) => req.url.includes(route))
+  ) {
     auth().protect();
   }
 });
